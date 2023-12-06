13688b4490fb7ef8b017a5f8103e79fcd752e51a
Arthur Bouffay <80390248+Tempauras@users.noreply.github.com>
2023-12-06 12:18:45 +0100

    Player movement and camera

--- Assets/_Scripts/Player/Player.cs
+++ Assets/_Scripts/Player/Player.cs
@@ -6,15 +6,47 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float _speed = 20.0f;
 
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D _rb;
+    private Player_InputActions _inputAction;
+    private SpriteRenderer _spriteRenderer;
+    private Vector2 _playerDirection = Vector2.zero;
+    private Animator _animator;
+    private void Awake()
     {
-        
+        _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+        _inputAction = new Player_InputActions();
+        _inputAction.Enable();
     }
 
-    // Update is called once per frame
-    void Update()
+    // Start is called before the first frame update
+    void Start()
+    { 
+
+        _inputAction.Player.Move.performed += moving =>
+        {
+            _playerDirection = moving.ReadValue<Vector2>();
+        };
+        _inputAction.Player.Move.canceled += moving =>
+        {
+            _playerDirection = moving.ReadValue<Vector2>();
+        };
+    }
+
+    private void FixedUpdate()
     {
-        
+        Debug.Log(_playerDirection);
+        _rb.velocity = _playerDirection * _speed;
+        if (_playerDirection.x > 0) 
+        {
+            _spriteRenderer.flipX = false;
+        }
+        if (_playerDirection.x < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
+        _animator.SetBool("isMoving", _playerDirection == Vector2.zero ? false : true);
     }
 }
