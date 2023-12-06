d2dbecd2b2ec3e23f7064b75dd9e8cc3195e4787
MrNyvle <elvynwakeford@gmail.com>
2023-12-06 15:17:50 +0100

    Seed implemented

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Serialization;
+using Random = UnityEngine.Random;
 
 namespace _Scripts
 {
@@ -12,10 +13,13 @@ namespace _Scripts
         [SerializeField] private Player player;
 
         [SerializeField] private Chest[] _chests = {};
+        [SerializeField] private int Seed;
 
 
         private void Start()
         {
+            Random.InitState(Seed);
+            
             _chests[0].SetupChest("A", new Item("Sword", 10));
         }
 
