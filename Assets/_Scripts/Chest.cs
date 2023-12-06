fd79096b062f46a7a127ad2dd658ee83dad05fc1
MrNyvle <elvynwakeford@gmail.com>
2023-12-06 13:54:51 +0100

    TMP et NaughtyAttributes et chest

--- Assets/_Scripts/Chest.cs
+++ Assets/_Scripts/Chest.cs
@@ -1,22 +1,93 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
+using System.Linq;
+using NaughtyAttributes;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
-public class Chest : MonoBehaviour
+namespace _Scripts
 {
-    
-    
-    
-    
-    // Start is called before the first frame update
-    void Start()
+    public class Chest : MonoBehaviour
     {
-        
-    }
+        private Item _item;
+        private bool _isOpen;
+        private bool _canBeOpened;
+        private Animator _animator;
+        private SpriteRenderer _spriteRenderer;
+        [SerializeField] private Sprite spriteChestOpened;
+        [SerializeField] private GameObject[] uis;
 
-    // Update is called once per frame
-    void Update()
-    {
+        private readonly Chest[] _chestsForOpen;
+
+        private void Awake()
+        {
+            _animator = GetComponent<Animator>();
+            _spriteRenderer = GetComponent<SpriteRenderer>();
+        }
         
+        public Chest(String name, Item item, Chest[] chestsForOpen = null)
+        {
+            gameObject.name = name;
+            _item = item;
+            _chestsForOpen = chestsForOpen;
+            _canBeOpened = chestsForOpen != null;
+        }
+
+        [Button("toggle")]
+        public void ToggleMessage()
+        {
+            int index = 1;
+            (bool, string) checkValues = CheckForPredecessors();
+            
+            if (checkValues.Item1)
+            {
+                index = 0;
+            }
+            uis[index].gameObject.SetActive(!uis[index].gameObject.activeSelf);
+            TMP_Text tmpText = uis[index].GetComponent<TMP_Text>();
+
+            tmpText.text = "Open" + checkValues.Item2;
+
+        }
+
+        private (bool,string) CheckForPredecessors()
+        {
+            string notOpenedChests = "";
+            if (_chestsForOpen == null)
+            {
+                return (true,notOpenedChests);
+            }
+            
+            foreach (var chest in _chestsForOpen)
+            {
+                if (!chest._isOpen)
+                {
+                    notOpenedChests += chest.gameObject.name.ToUpper();
+                }
+            }
+
+            if (notOpenedChests != "") 
+            {
+                return (true,notOpenedChests);
+            }
+
+            return (false,notOpenedChests);
+        }
+
+        public void OpenChest()
+        {
+            _animator.enabled = true;
+        }
+
+
+        public void ChestOpened()
+        {
+            _animator.enabled = false;
+
+            _spriteRenderer.sprite = spriteChestOpened;
+
+            GameManager.instance.GiveItemToPlayer(_item);
+        }
     }
 }
---
+++ Assets/_Scripts/GameManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace _Scripts
+{
+    public class GameManager : Singleton<GameManager>
+    {
+
+        [SerializeField] private Player player;
+
+
+        public void GiveItemToPlayer(Item item)
+        {
+            
+            
+            
+        }
+
+    }
+}
